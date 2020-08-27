d4dc6bf913dd0403abb7dc004aaa595a72c03133
antonthunmanjonsson <jonssonanton@hotmail.com>
2020-08-27 09:27:14 +0200

    Solution02 Example04 & 05

---
+++ Session02/Session02/Solution02Example05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution02Example05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange vattentemperatur i grader C:");
+
+            var input = Console.ReadLine();
+            var integer = Convert.ToInt32(input);
+
+            string waterLabel = integer >= 27 ? "Går att bada" : "Går inte att bada";
+
+            // Är samma sak som
+            //if (integer > 27)
+            //{
+            //    waterLabel = "Går att bada";
+            //}
+            //else
+            //{
+            //    waterLabel = "Går inte att bada";
+            //}
+
+
+
+            Console.WriteLine(waterLabel + " i havet");
+        }
+    }
+}
