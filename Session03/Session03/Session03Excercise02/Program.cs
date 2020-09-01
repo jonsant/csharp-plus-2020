aec7b6af279322d7c5d24e8d49f786865901ae30
antonthunmanjonsson <jonssonanton@hotmail.com>
2020-09-01 11:33:56 +0200

    Session03Excercise02

--- Session03/Session03/Session03Excercise02/Program.cs
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Session03Excercise02
 {
@@ -17,11 +19,40 @@ namespace Session03Excercise02
             Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
             var input = Console.ReadLine();
             var inputArray = input.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            int? sum = 0;
+            int? average = null;
+            int? maxValue = null;
+            int? minValue = null;
+            List<int> numbers = new List<int>();
+            int[] intArray = new int[] { };
 
-            foreach (var number in inputArray)
+            for (int stringNum = 0; stringNum < inputArray.Length; stringNum++)
             {
-                Console.WriteLine("Värdet är " + number);
+                int number;
+                if (Int32.TryParse(inputArray[stringNum], out number))
+                {
+                    numbers.Add(number);
+                    sum += number;
+                }
             }
+
+            intArray = numbers.ToArray();
+
+            try
+            {
+                maxValue = intArray.Max();
+                minValue = intArray.Min();
+                average = sum / intArray.Length;
+
+            } catch (Exception e)
+            {
+
+            }
+
+            Console.WriteLine($"\nNumber of values: {intArray.Length}");
+            Console.WriteLine($"Max value: {maxValue}");
+            Console.WriteLine($"Min value: {minValue}");
+            Console.WriteLine($"Average: {average}");
 
         }
     }
